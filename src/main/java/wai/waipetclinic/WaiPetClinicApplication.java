610a0a08341fbb9631a626b6ff077ee8009bf508
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-09-21 15:41:07 +0200

    Initial Project Add

---
+++ src/main/java/wai/waipetclinic/WaiPetClinicApplication.java
@@ -0,0 +1,13 @@
+package wai.waipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaiPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaiPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wai/waipetclinic/WaiPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package wai.waipetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaiPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
