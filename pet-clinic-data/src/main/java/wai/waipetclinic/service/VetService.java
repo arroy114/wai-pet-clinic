63ef541b13f137023725af4606d11f2b9d6377dc
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-09-24 09:16:26 +0200

    Create Interface for Pet Clinic Services. #5 closed.

---
+++ pet-clinic-data/src/main/java/wai/waipetclinic/service/VetService.java
@@ -0,0 +1,15 @@
+package wai.waipetclinic.service;
+
+
+import wai.waipetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
