eff02dbf50cf856d1c31e9c51f51e002b9c21065
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-09-25 16:04:38 +0200

    Adding Vet list and updated controller. Closes #15

--- pet-clinic-web/src/main/java/wai/waipetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/wai/waipetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package wai.waipetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import wai.waipetclinic.service.VetService;
 
 @Controller
 public class VetController {
 
+    private VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
 
         return "vets/index";
     }
