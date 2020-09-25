29b890a8307cba6a576d1efee94205c2a5b16399
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-09-25 15:57:43 +0200

    Adding Owner list and controller. Closes #16

--- pet-clinic-web/src/main/java/wai/waipetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/wai/waipetclinic/controllers/OwnerController.java
@@ -1,15 +1,24 @@
 package wai.waipetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import wai.waipetclinic.service.OwnerService;
 
 @RequestMapping("/owners") //pre-fixing mapping for the whole class
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     //due to pre-fixing the mapping here is actually "/owners", "/owners/index", "/owners/index.html"
     @RequestMapping({"", "/index", "/index.html"})
-    public String listOwner(){
+    public String listOwner(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
