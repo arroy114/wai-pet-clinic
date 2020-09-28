8d81a0e028cde91d51dfa158de8ffc343c869f7b
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-09-28 21:38:38 +0200

    Added Pets and Contact info to owner on startup Closes #33

--- pet-clinic-data/src/main/java/wai/waipetclinic/service/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/wai/waipetclinic/service/map/OwnerServiceMap.java
@@ -2,12 +2,23 @@ package wai.waipetclinic.service.map;
 
 import org.springframework.stereotype.Service;
 import wai.waipetclinic.model.Owner;
+import wai.waipetclinic.model.Pet;
 import wai.waipetclinic.service.OwnerService;
+import wai.waipetclinic.service.PetService;
+import wai.waipetclinic.service.PetTypeService;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -21,7 +32,32 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner owner) {
-        return super.save(owner);
+        Owner savedOwner = null;
+
+        //Save all petType that owner have and does exist in petType Map service before
+        if(owner != null){
+            if (owner.getPets() != null){
+                owner.getPets().forEach(pet ->{
+                    if(pet.getPetType() != null){
+                        if(pet.getPetType().getId() == null) { //check if petType exists in Map service
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    } else {
+                        throw new RuntimeException("Pet Type is required");
+                    }
+
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+
+            return super.save(owner);
+
+        } else {
+            return null;
+        }
     }
 
     @Override
--- pet-clinic-web/src/main/java/wai/waipetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/wai/waipetclinic/bootstrap/DataLoader.java
@@ -3,11 +3,14 @@ package wai.waipetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import wai.waipetclinic.model.Owner;
+import wai.waipetclinic.model.Pet;
 import wai.waipetclinic.model.PetType;
 import wai.waipetclinic.model.Vet;
 import wai.waipetclinic.service.OwnerService;
 import wai.waipetclinic.service.PetTypeService;
 import wai.waipetclinic.service.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +39,33 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 street");
+        owner1.setCity("Hong Kong");
+        owner1.setTelephone("11223344");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType((savedDogPetType));
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthday(LocalDate.now());
+        mikesPet.setName("Bingo");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("555 street");
+        owner2.setCity("New York");
+        owner2.setTelephone("22223333");
+
+        Pet fionaCat = new Pet();
+        fionaCat.setName("Just cat");
+        fionaCat.setOwner(owner2);
+        fionaCat.setBirthday(LocalDate.now());
+        fionaCat.setPetType(savedCattType);
+        owner2.getPets().add(fionaCat);
+
 
         ownerService.save(owner2);
 
