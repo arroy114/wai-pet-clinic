b17b9a9e86fe28afb0ebd387dcebaa8770017ed3
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-09-28 21:08:17 +0200

    Added PetTypes in boostrap Closes #32

--- pet-clinic-web/src/main/java/wai/waipetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/wai/waipetclinic/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package wai.waipetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import wai.waipetclinic.model.Owner;
+import wai.waipetclinic.model.PetType;
 import wai.waipetclinic.model.Vet;
 import wai.waipetclinic.service.OwnerService;
+import wai.waipetclinic.service.PetTypeService;
 import wai.waipetclinic.service.VetService;
 
 @Component
@@ -12,16 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCattType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
