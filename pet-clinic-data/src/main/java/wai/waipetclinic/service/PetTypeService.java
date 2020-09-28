2fe1876c4e8b7138b1abe2fe606823f6efc7396c
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-09-28 21:01:39 +0200

    Impl PetTypeServiceMap Closes #29

---
+++ pet-clinic-data/src/main/java/wai/waipetclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package wai.waipetclinic.service;
+
+import wai.waipetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/wai/waipetclinic/service/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package wai.waipetclinic.service.map;
+
+import org.springframework.stereotype.Service;
+import wai.waipetclinic.model.PetType;
+import wai.waipetclinic.service.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petType;
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
